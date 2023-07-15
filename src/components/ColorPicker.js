fd11206744a93310ebf39bca3db87d00f7066f98
dylcorman <dylancorman8@gmail.com>
2023-07-15 13:15:09 -0400

    chore: update to main branch

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,19 +1,33 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
-import { SketchPicker } from 'react-color';
+import React, { useState, useContext } from "react";
+import { SketchPicker } from "react-color";
 
-const ColorPicker = () => {
-    const [color, setColor] = useState('#fff');
+const ColorPicker = ({ newProperty }) => {
+    //const setCurrentProperties = useContext(currentProperties);
+    const [color, setColor] = useState("#fff");
+    const [height, setHeight] = useState("");
 
     const handleChangeComplete = (color) => {
         setColor(color.hex);
     };
 
+    function handleNewProperty(event, property) {
+        let newPropertys = {};
+        newPropertys[property] = event.target.value;
+        newProperty(newPropertys); //Calls home page's newProperty function and passes the as a paramater
+    }
+
     return (
-        <div style={{ position: 'absolute', top: '60px', right: '10px' }}>
+        <div style={{ position: "absolute", top: "60px", right: "10px" }}>
             <SketchPicker color={color} onChangeComplete={handleChangeComplete} />
             <h2>You picked {color}</h2>
+            <input
+                type="text"
+                placeholder="height"
+                onChange={(event) => handleNewProperty(event, "height")}
+                className="text-black"
+            />
         </div>
     );
 };
