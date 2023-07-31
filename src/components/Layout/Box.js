3552aa6a3e7c29dd9e83d36bbf6ef6b68b699bd8
LeifCastle <leifurcastle@gmail.com>
2023-07-30 21:40:49 -0700

    feat: user's theme's state properties set at default figured out...messy but can refactor

--- src/components/Layout/Box.js
+++ src/components/Layout/Box.js
@@ -1,5 +1,5 @@
 import { Rnd } from "react-rnd";
-import { useContext, useState, useEffect, cloneElement } from "react";
+import { useContext, useState, useRef, useEffect } from "react";
 
 import { currentProperties } from "../../app/context";
 import { currentElementContext } from "../../app/context";
@@ -14,11 +14,41 @@ export default function Box({ id, name, setCurrentElement }) {
   const [y, setY] = useState(100); //The box's y position relative to its parent
   const [width, setWidth] = useState("50px"); //The box's width
   const [height, setHeight] = useState("50px"); //The box's height
-  const [first, setFirst] = useState(true);
+  const initial = useRef({});
+  const ran = useRef(false);
+  const elRef = useRef([]);
+
+  //Maybe don't even need this but check if PC(console.log) is sending off both boxes or just one i think it might be one cuz it doesn't even fucking show up yk
+  if (ran.current === false) {
+    let refArray = {};
+    console.log("NPP: ", newProperties);
+    for (let element in newProperties) {
+      refArray[element] = true;
+    }
+    elRef.current = refArray;
+    console.log("ElRef: ", elRef.current);
+    for (let element in newProperties) {
+      initial.current[element] = true;
+      ran.current = true;
+    }
+  }
+
+  for (let element in newProperties) {
+    if (element === id && elRef.current[element] === true) {
+      for (let property in newProperties[element]) {
+        if (property === "width") {
+          console.log(element, "'s width property set");
+          setWidth(newProperties[element][property]);
+        }
+      }
+      elRef.current[element] = false;
+    }
+  }
 
   //----Element's default properties
   let defaultProperties = {
     backgroundColor: "#2A2A2A",
+    width: "200px",
   };
 
   //----If this element's id matches the theme element's id assign new property values,
@@ -27,6 +57,12 @@ export default function Box({ id, name, setCurrentElement }) {
       for (let property in newProperties[element]) {
         if (property !== "width" && property != "height") {
           defaultProperties[property] = newProperties[element][property];
+        } else if (property === "width" && 1 > 9) {
+          console.log(
+            "Should be false after one run: ",
+            newProperties[element]["initial"]
+          );
+          setWidth(newProperties[element][property]);
         }
       }
     }
@@ -39,20 +75,27 @@ export default function Box({ id, name, setCurrentElement }) {
     cEPropsId = currentElement.props.id;
   }
 
-  useEffect(() => {
-    for (let element in newProperties) {
-      //If this element's id matches the theme element's id and is current selected assign new height/width
-      if (element === id && element === cEPropsId) {
-        for (let property in newProperties[element]) {
-          if (property === "width") {
-            setWidth(newProperties[element][property]);
-          } else if (property === "height") {
-            setHeight(newProperties[element][property]);
-          }
-        }
-      }
-    }
-  }, [newProperties, cEPropsId, id]);
+  // useEffect(() => {
+  //   for (let element in newProperties) {
+  //     //If this element's id matches the theme element's id and is current selected assign new height/width
+  //     if (element === id && element === cEPropsId) {
+  //       for (let property in newProperties[element]) {
+  //         if (property === "width") {
+  //           if (
+  //             property === "width" &&
+  //             newProperties[element]["initial"] === true
+  //           ) {
+  //             setWidth(newProperties[element][property]);
+  //             setInitial(element);
+  //           }
+  //         }
+  //         //else if (property === "height") {
+  //         //   setHeight(newProperties[element][property]);
+  //         // }
+  //       }
+  //     }
+  //   }
+  // }, [newProperties, cEPropsId, id]);
 
   //----Component to return (defined as a variable to allow the currentElement state to access it)
   let component = (
